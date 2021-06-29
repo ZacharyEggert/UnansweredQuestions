a811ba232fe5e93dbc4b0295728b197dc4d11867
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-06-28 19:52:22 -0700

    return not render

--- client/src/components/SuggestionForm/index.js
+++ client/src/components/SuggestionForm/index.js
@@ -18,122 +18,121 @@ const SuggestionForm = () => {
         });
     }
 
-    render() {
-        return (
-            <form>
-                <div className="mb-4 form-group sm:flex sm:justify-between">
-                    <label
-                        for="suggestion"
-                        className="w-full sm:w-6/12 inline-block"
-                        id="listbox-label"
-                    >
-                        What would you like to make a suggestion for?
-                    </label>
-                    <select
-                        value={state["suggestion-type"]}
-                        name="suggestion-type"
-                        id="suggestion-type"
-                        className="w-full sm:w-5/12 inline-block form-select mt-1 text-black p-1"
-                        placeholder="Choose subject..."
-                    >
-                        <option
-                            className="text-black pl-1"
-                            value=""
-                            disabled
-                            selected
-                        >
-                            Choose subject...
-                        </option>
-                        <option value="philosopher" className="text-black pl-1">
-                            Philosopher
-                        </option>
-                        <option value="quote" className="text-black pl-1">
-                            Quote
-                        </option>
-                        <option value="question" className="text-black pl-1">
-                            Daily Question
-                        </option>
-                    </select>
-                </div>
-                <div
-                    className="philosopher box2 mb-4 sm:flex sm:justify-between"
-                    id="otherFieldDiv"
+    
+    return (
+        <form>
+            <div className="mb-4 form-group sm:flex sm:justify-between">
+                <label
+                    for="suggestion"
+                    className="inline-block w-full sm:w-6/12"
+                    id="listbox-label"
                 >
-                    <label for="phil-name" className="sm:w-4/12 inline-block">
+                    What would you like to make a suggestion for?
+                </label>
+                <select
+                    value={state["suggestion-type"]}
+                    name="suggestion-type"
+                    id="suggestion-type"
+                    className="inline-block w-full p-1 mt-1 text-black sm:w-5/12 form-select"
+                    placeholder="Choose subject..."
+                >
+                    <option
+                        className="pl-1 text-black"
+                        value=""
+                        disabled
+                        selected
+                    >
+                        Choose subject...
+                    </option>
+                    <option value="philosopher" className="pl-1 text-black">
+                        Philosopher
+                    </option>
+                    <option value="quote" className="pl-1 text-black">
+                        Quote
+                    </option>
+                    <option value="question" className="pl-1 text-black">
+                        Daily Question
+                    </option>
+                </select>
+            </div>
+            <div
+                className="mb-4 philosopher box2 sm:flex sm:justify-between"
+                id="otherFieldDiv"
+            >
+                <label for="phil-name" className="inline-block sm:w-4/12">
+                    Philosopher Name:
+                </label>
+                <input
+                    value={state["phil-name"]}
+                    className="p-1 text-black"
+                    type="text"
+                    name="phil-name"
+                    id="phil-name"
+                    placeholder="Enter philosopher name..."
+                    required
+                />
+            </div>
+            <div className="mb-4 quote box2" id="otherFieldGroupDiv">
+                <div className="mb-4 col-6 sm:flex sm:justify-between">
+                    <label
+                        for="q-phil-name"
+                        className="inline-block sm:w-4/12"
+                    >
                         Philosopher Name:
                     </label>
                     <input
-                        value={state["phil-name"]}
-                        className="text-black p-1"
+                        value={state["q-phil-name"]}
+                        name="q-phil-name"
                         type="text"
-                        name="phil-name"
-                        id="phil-name"
+                        className="p-1 text-black form-control"
+                        id="q-phil-name"
                         placeholder="Enter philosopher name..."
                         required
                     />
                 </div>
-                <div className="quote box2 mb-4" id="otherFieldGroupDiv">
-                    <div className="col-6 mb-4 sm:flex sm:justify-between">
-                        <label
-                            for="q-phil-name"
-                            className="sm:w-4/12 inline-block"
-                        >
-                            Philosopher Name:
-                        </label>
-                        <input
-                            value={state["q-phil-name"]}
-                            name="q-phil-name"
-                            type="text"
-                            className="form-control text-black p-1"
-                            id="q-phil-name"
-                            placeholder="Enter philosopher name..."
-                            required
-                        />
-                    </div>
-                    <div className="col-6 sm:flex sm:justify-between">
-                        <label for="quote" className="sm:w-4/12 inline-block">
-                            Quote
-                        </label>
-                        <input
-                            value={state["quote"]}
-                            name="quote"
-                            type="text"
-                            className="form-control text-black p-1"
-                            id="quote"
-                            placeholder="Enter quote..."
-                            required
-                        />
-                    </div>
-                </div>
-                <div
-                    className="question box2 mb-4 sm:flex sm:justify-between"
-                    id="otherFieldDiv3"
-                >
-                    <label for="quest" className="sm:w-4/12 inline-block">
-                        Question:
+                <div className="col-6 sm:flex sm:justify-between">
+                    <label for="quote" className="inline-block sm:w-4/12">
+                        Quote
                     </label>
                     <input
-                        className="text-black p-1"
+                        value={state["quote"]}
+                        name="quote"
                         type="text"
-                        name="quest"
-                        value={state["quest"]}
-                        id="quest"
-                        placeholder="Enter question..."
+                        className="p-1 text-black form-control"
+                        id="quote"
+                        placeholder="Enter quote..."
                         required
                     />
                 </div>
-                <span className="w-8/12 inline-block text-right contents">
-                    <button
-                        id="suggestion"
-                        type="submit"
-                        className="text-white rounded-md px-3  border-white border-2 border-solid whitespace-nowrap md:w-3/12 min-w-min bg-[#4d83a3] w-full items-center"
-                    >
-                        Submit
-                    </button>
-                </span>
-            </form>
-        );
-    }
+            </div>
+            <div
+                className="mb-4 question box2 sm:flex sm:justify-between"
+                id="otherFieldDiv3"
+            >
+                <label for="quest" className="inline-block sm:w-4/12">
+                    Question:
+                </label>
+                <input
+                    className="p-1 text-black"
+                    type="text"
+                    name="quest"
+                    value={state["quest"]}
+                    id="quest"
+                    placeholder="Enter question..."
+                    required
+                />
+            </div>
+            <span className="inline-block w-8/12 text-right contents">
+                <button
+                    id="suggestion"
+                    type="submit"
+                    className="text-white rounded-md px-3  border-white border-2 border-solid whitespace-nowrap md:w-3/12 min-w-min bg-[#4d83a3] w-full items-center"
+                >
+                    Submit
+                </button>
+            </span>
+        </form>
+    );
 }
 
 export default SuggestionForm;
