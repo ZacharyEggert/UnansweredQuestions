a5a15e72663ca95af94a409eeb7284e9c4b5bc56
Alicia Breyer <alicia_breyer@yahoo.com>
2021-07-03 14:01:13 -0700

    started login form with axios

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -8,4 +8,12 @@ const getPhilosophers = () => {
     });
 };
 
+const getUser = () => {
+    return axios.post('/api/login').then((req, res) => {
+        if (res.status < 300) {
+            return res;
+        }
+    });
+};
+
 export { getPhilosophers };
