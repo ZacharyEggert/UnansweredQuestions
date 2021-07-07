c7fb9bf7e2792607b8c83c2e6a188d810687a047
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-07 12:48:21 -0700

    serve client app

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,13 @@ const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
 
-router.use('/', homeRoutes);
+router.get("*", (req, res) => {
+    try{
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    }catch(err){
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
