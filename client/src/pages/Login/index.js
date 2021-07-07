c222ab638693e4c46fbbf6cc6d4aa5e9bc284bc7
Trey Love <theendurinpupil@gmail.com>
2021-07-06 17:44:57 -0700

    changed styling on login page to try to address input opacity

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -10,12 +10,12 @@ const Login = () => {
 
                 <div className='py-8'></div>
 
-                <div className='flex flex-row flex-wrap bg-black m-14 p-7 bg-opacity-40'>
+                <div className='flex flex-row flex-wrap bg-black m-14 p-7 bg-opacity-60'>
                     <div className='flex-1 flex-shrink-0 w-6/12 pr-1 min-w-max'>
                         <div className=''>
                             <h1 className='text-4xl underline text-white'>Log in</h1>
                         </div>
-                        <div className='w-auto max-w-full p-8 py-8 bg-white form bg-opacity-10 h-72'>
+                        <div className='w-auto max-w-full p-8 py-8 bg-white  bg-opacity-10 h-72 '>
                             <form>
                                 <label
                                     htmlFor='email'
@@ -26,7 +26,7 @@ const Login = () => {
                                     name='email'
                                     id=''
                                     type='text'
-                                    className='max-w-full pl-1 my-3 text-black'
+                                    className='max-w-full pl-1 my-3 text-black bg-white opacity-100 '
                                     hidden
                                 />
                                 <br />
@@ -38,7 +38,7 @@ const Login = () => {
                                     name='username'
                                     id='username'
                                     type='text'
-                                    className='max-w-full pl-1 my-3 text-black'
+                                    className='max-w-full pl-1 my-3 text-black bg-white opacity-100'
                                 />
                                 <br />
                                 <label
@@ -49,7 +49,7 @@ const Login = () => {
                                     name='password'
                                     id='password'
                                     type='text'
-                                    className='max-w-full pl-1 my-3 text-black bg-white'
+                                    className='max-w-full pl-1 my-3 text-black bg-white opacity-100'
                                 />
                                 <br />
                                 <span className='inline-block w-8/12 text-right'><button
@@ -64,14 +64,14 @@ const Login = () => {
                         <div className='signup-label'>
                             <h1 className='text-4xl underline text-white'>Sign up</h1>
                         </div>
-                        <div className='w-auto max-w-full p-8 py-8 bg-white form bg-opacity-5 h-72'>
+                        <div className='w-auto max-w-full p-8 py-8 bg-white form bg-opacity-10 h-72'>
                             <form>
                                 <label htmlFor='email' className='inline-block w-4/12 my-3'>Email</label>
                                 <input
                                     name='email'
                                     id='signupemail'
                                     type='text'
-                                    className='max-w-full pl-1 my-3 text-black '
+                                    className='max-w-full pl-1 my-3 text-black bg-white opacity-100'
                                 />
                                 <br />
                                 <label
@@ -82,7 +82,7 @@ const Login = () => {
                                     name='username'
                                     id='signupusername'
                                     type='text'
-                                    className='max-w-full pl-1 my-3 text-black '
+                                    className='max-w-full pl-1 my-3 text-black bg-white opacity-100'
                                 />
                                 <br />
                                 <label
@@ -93,7 +93,7 @@ const Login = () => {
                                     name='password'
                                     id='signuppassword'
                                     type='text'
-                                    className='max-w-full pl-1 my-3 text-black '
+                                    className='max-w-full pl-1 my-3 text-black bg-white opacity-100'
                                 />
                                 <br />
                                 <span className='inline-block w-8/12 text-right'><button
