d627bd6913dd4fba54db4ccbc28a61a4bea20eaa
Trey Love <theendurinpupil@gmail.com>
2021-07-10 11:38:47 -0700

    updated API routes

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -2,11 +2,43 @@ import axios from "axios"
 
 const getPhilosophers = () => {
     return axios.get('/api/philosophers')
-    .then(response => {
-        if(response.status < 300 ){
-            return response
-        }
-    })
+        .then(response => {
+            if (response.status < 300) {
+                return response
+            }
+        })
+}
+// get all polls
+const getPolls = () => {
+    return axios.get('/api/polls')
+        .then(response => {
+            if (response.status < 300) {
+                return response
+            }
+        })
+}
+//get all quotes 
+const getQuotes = () => {
+    return axios.get('/api/quotes')
+        .then(response => {
+            if (response.status < 300) {
+                return response
+            }
+
+        })
+}
+//get current qotd info eg. question,comments
+//need to update the logic for the route (homeroutes line 116) to start at a later date and reset when it runs out of daily questions
+//need to update the route to put info into state
+const getQotd = () => {
+    return axios.get('/api/qotd/')
+        .then(response => {
+            if (response.status < 300) {
+                return response
+            }
+        })
 }
 
-export { getPhilosophers }
+
+
+export { getPhilosophers, getPolls, getQuotes, getQotd }
