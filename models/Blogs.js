a5378c3b49f924ff76e792fba52eff7ac2b39137
Trey Love <theendurinpupil@gmail.com>
2021-07-12 20:43:46 -0700

    made basic layout of new models

---
+++ models/Blogs.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Blogs extends Model { }
+
+
+
+Blogs.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                length: {
+                    min: 10,
+                    max: 2000
+                }
+            }
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+
+
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Blogs',
+    }
+);
+
+module.exports = Blogs;
---
+++ models/News.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class News extends Model { }
+
+News.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'News',
+    }
+
+)
+
+module.exports = News;
+
