fe9ad249879ac3d0ad55bfe2dde0847df66758e5
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-13 22:38:11 -0700

    OWL CAROUSEL

--- client/src/components/OwlCarousel/index.js
+++ client/src/components/OwlCarousel/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import Carousel from 'react-owl-carousel';
 
 import OwlCarouselSlide from '../OwlCarouselSlide';
 
 import { useGlobalContext } from '../../util/GlobalState';
+import '../../pages/FrontPage/owl.carousel.css'
 
 const OwlCarousel = () => {
     // eslint-disable-next-line no-unused-vars
     const [globalState, dispatch] = useGlobalContext();
 
-    const { carouselQuotes } = globalState;
 
     return (
-        <div className="owl-carousel owl-theme full-screen bg-carousel-1 bg-carousel-2 bg-carousel-3 bg-none">
-            {carouselQuotes.map((quote, index) => (
+        <div className="bg-carousel-1 bg-carousel-2 bg-carousel-3 bg-none">
+            <Carousel className='owl-carousel' loop items={1} dots dotsContainer='dots' autoplay autoplaySpeed={2000} autoplayTimeout={7500}>
+            {globalState.carouselQuotes.map((quote, index) => (
                 <OwlCarouselSlide quote={quote} key={quote.id} index={index} />
             ))}
+            </Carousel>
         </div>
     );
 };
