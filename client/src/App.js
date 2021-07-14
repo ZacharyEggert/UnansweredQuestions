c6573f174c2c10518dfa60c6fca78b273f720781
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-13 22:38:01 -0700

    move wrapper around APP.js so I can do a one-time data grab

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AllPhilosophers from './pages/AllPhilosophers';
 import Chat from './pages/Chat';
@@ -9,15 +9,53 @@ import Quiz from './pages/Quiz';
 import Qotd from './pages/Qotd';
 import Login from './pages/Login';
 import Polls from './pages/Polls';
-
-import { GlobalProvider } from './util/GlobalState';
 import NavBar from './components/NavBar';
-
 import OnePhilosopher from './pages/OnePhilosopher';
+import { useGlobalContext } from './util/GlobalState';
+import {getPhilosophers, getPolls, getQuotes} from './util/API';
+import Helpers from './util/Helpers';
+
 
 const App = () => {
+
+    // eslint-disable-next-line no-unused-vars
+    const [globalState, dispatch] = useGlobalContext();
+
+    useEffect(() => {
+        // Set the global state
+        getPhilosophers()
+        .then(philosophers => {
+            dispatch({type: 'addPhilosophersBulk', data: philosophers.data});
+        });
+
+        getPolls()
+        .then(polls => {
+            dispatch({type: 'setPolls', data: polls.data});
+        });
+
+        getQuotes()
+        .then(quotes => {
+            console.log(quotes);
+            let quoteSet = quotes.data;
+            let pickedQuotes = [];
+            Helpers.randomPicks(quoteSet.length,6).forEach(quote => {
+                pickedQuotes.push(quoteSet[quote]);
+            })
+            return pickedQuotes;
+        }).then(pickedQuotes => {
+            setTimeout(() => {
+                dispatch({type: 'setQuotes', data: pickedQuotes});
+            }, 20);
+        });
+
+        console.log(globalState);
+
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
+
     return (
-        <GlobalProvider>
+        
             <Router>
                 <NavBar />
                 <Switch>
@@ -47,7 +85,6 @@ const App = () => {
                     <Route exact path="/quiz" component={Quiz} />
                 </Switch>
             </Router>
-        </GlobalProvider>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
+import { GlobalProvider } from './util/GlobalState';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GlobalProvider><App /></GlobalProvider>, document.getElementById('root'));
