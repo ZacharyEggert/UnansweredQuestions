ab80b5e8091ddaca06233ff2dbe3a9913027aa72
Trey Love <theendurinpupil@gmail.com>
2021-07-16 15:49:07 -0700

    updated comment model

--- models/Comments.js
+++ models/Comments.js
@@ -22,7 +22,11 @@ Comments.init(
         },
         daily_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
         },
         likes: {
             type: DataTypes.INTEGER,
