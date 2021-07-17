95c4d10684c02e5d6a63907148e3cc0cbb6955b1
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-17 10:58:38 -0700

    Nav Style

--- client/src/components/SuggestionCard/index.js
+++ client/src/components/SuggestionCard/index.js
@@ -16,7 +16,7 @@ const SuggestionCard = () => {
                         <div className="flex flex-col items-center justify-center flex-1 w-full sm:pl-4">
                             <Link
                                 to="/suggestions"
-                                className="w-8/12 mx-auto text-center sm:w-full button"
+                                className="w-8/12 mx-auto text-center sm:w-full lg:w-8/12 button"
                             >
                                 Make a suggestion
                             </Link>
