3606c66b3319e36e147e466e466a354ef14fa7c2
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-17 14:14:44 -0700

    fill data into database as requested

--- server.js
+++ server.js
@@ -95,6 +95,6 @@ io.on('connection', (socket) => {
     });
 });
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false, alter:true }).then(() => {
     server.listen(PORT, () => console.log('Now listening on port ' + PORT));
 });
