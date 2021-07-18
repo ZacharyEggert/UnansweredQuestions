e4a43ce30e0efbe0b4ae0ae9298753b6ac69bac6
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-17 22:55:37 -0700

    cards, mvp

--- client/src/components/SuggestionReviewCard/index.js
+++ client/src/components/SuggestionReviewCard/index.js
@@ -1,19 +1,73 @@
 import React from 'react';
 
+const SuggestionReviewQuestion = ({props}) => {
+    const {suggestion} = props;
+
+    const {id, name, question, quote, quotephilname, } = suggestion;
+
+    return (
+        <div className="py-1 px-1 bg-[#FFF2] my-1 text-center">
+            {question}
+        </div>
+    );
+};
+
+
+const SuggestionReviewQuote = ({props}) => {
+    const {suggestion} = props;
+
+    const {id, name, question, quote, quotephilname, } = suggestion;
+
+    return (
+        <div className="py-1 px-1 bg-[#FFF2] my-1 text-center">
+            {quote}
+        </div>
+    );
+};
+
+
+const SuggestionReviewPhilosopher = ({props}) => {
+    const {suggestion} = props;
+
+    const {id, name, question, quote, quotephilname, } = suggestion;
+
+    return (
+        <div className="py-1 px-1 bg-[#FFF2] my-1 text-center">
+            {name}
+        </div>
+    );
+};
+
+
 const SuggestionReviewCard = (props) => {
     const {suggestion, isChecked} = props;
 
-    const {id, name, question, quote, quotephilname, sugg_type} = suggestion;
+    const {sugg_type} = suggestion;
 
     if(!isChecked){
         return(
             null)
     }
-    return (
-        <div className="py-1 px-1 bg-[#FFF2] my-1">
-            <div className="">hello</div>
-        </div>
-    );
+    switch(sugg_type){
+        case 'question':
+            return(
+                <SuggestionReviewQuestion props={props}/>
+            )
+        case 'quote':
+            return(
+                <SuggestionReviewQuote props={props}/>
+            )
+        case 'philosopher':
+            return(
+                <SuggestionReviewPhilosopher props={props}/>
+            )
+        default:
+            return (
+                <div className="py-1 px-1 bg-[#FFF2] my-1 text-center">
+                    ERROR
+                </div>
+            );
+    }
 };
 
 export default SuggestionReviewCard;
