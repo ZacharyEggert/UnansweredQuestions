797ce7d3825a4583ff642ea463f11e05ffce7006
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-18 16:06:20 -0700

    warning elim

--- client/src/components/DashboardScreen/index.js
+++ client/src/components/DashboardScreen/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 const DashboardScreen = ({state}) => {
 
+    // eslint-disable-next-line no-unused-vars
     const { dashboardState, setDashboardState } = state;
 
     return (
