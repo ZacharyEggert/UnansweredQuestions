bbce6cbfeb5249f592add06da0e0fcf5892382b8
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-07-17 21:29:14 -0700

    server suggestion routes

--- controllers/api/suggestionRoutes.js
+++ controllers/api/suggestionRoutes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const { Suggestions } = require('../../models');
 
+router.get('/', (req, res) => {
+    Suggestions.findAll()
+        .then((suggestions) => {
+            res.json(suggestions);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+            console.error(err);
+        });
+});
+
 router.post('/', async (req, res) => {
     try {
         const newSuggestion = await Suggestions.create({
@@ -12,6 +23,26 @@ router.post('/', async (req, res) => {
         });
 
         res.status(200).json(newSuggestion);
+    } catch (err) {
+        res.status(500).json(err);
+        console.error(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        console.log('APPROVE THE SUGGESTION');
+    } catch (err) {
+        res.status(500).json(err);
+        console.error(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const suggestion = await Suggestions.findById(req.params.id);
+        await suggestion.destroy();
+        res.status(200).json(suggestion);
     } catch (err) {
         res.status(500).json(err);
         console.error(err);
