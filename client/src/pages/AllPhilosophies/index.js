df354fa3429934761a7319ebb7a72d9f9967cad0
Trey Love <theendurinpupil@gmail.com>
2021-07-18 17:09:02 -0700

    made slight adjustments to the all philosophies styling

--- client/src/pages/AllPhilosophies/index.js
+++ client/src/pages/AllPhilosophies/index.js
@@ -13,14 +13,17 @@ const AllPhilosophies = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-w-full min-h-full text-white bg-fixed bg-cover bg-driedflowers bg-fade">
-            <h1 className=" text-white my-3">Philosophies</h1>
-            <div className="py-14"></div>
-            <div className="box">
-                {/**<!-- cards for each philosophy  -->*/}
-                {Object.values(globalState.philosophies).map((philosophy) => {
-                    return <PhilosophyCard philosophy={philosophy} key={philosophy.id} />;
-                })}
 
+
+            <div className="py-14"></div>
+            <div >
+                <div className="box">
+                    {/**<!-- cards for each philosophy  -->*/}
+                    {Object.values(globalState.philosophies).map((philosophy) => {
+                        return <PhilosophyCard philosophy={philosophy} key={philosophy.id} />;
+                    })}
+
+                </div>
             </div>
             <SuggestionCard />
         </div>
