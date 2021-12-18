db8adf61be1d32956a57a6fa2c62d1079e3dacce
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-12-18 11:21:55 -0800

    move everything to old; init new NEXT project

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-const tailwindcss = require('tailwindcss');
 module.exports = {
-    plugins: [
-        require('tailwindcss')('tailwind.js'),
-        require('autoprefixer')(),
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+  content: ['src/**/*.{tsx,ts,css,scss,sass,md,mdx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
